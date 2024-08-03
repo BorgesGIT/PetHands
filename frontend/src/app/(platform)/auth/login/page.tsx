b43ed6806721d1a6d19ef4deed9546e5af089464
Gabriel Canteiro <gabrielcanteiropt@gmail.com>
2024-08-03 12:24:49 +0100

    login page

--- frontend/src/app/(platform)/auth/login/page.tsx
+++ frontend/src/app/(platform)/auth/login/page.tsx
@@ -1,5 +1,66 @@
-import React from "react";
+import Link from "next/link";
+import { Card, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Chrome } from "lucide-react";
 
-export default function LoginPage() {
-  return <div>AuthLogin</div>;
+export default function AuthLoginPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <div className="w-full max-w-md space-y-6">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold tracking-tight">
+            Sign in to your account
+          </h1>
+          <p className="mt-2 text-muted-foreground">
+            Don't have an account?{" "}
+            <Link
+              href="#"
+              className="font-medium text-primary hover:underline"
+              prefetch={false}
+            >
+              Register
+            </Link>
+          </p>
+        </div>
+        <Card>
+          <CardContent className="space-y-4">
+            <form>
+              <div className="my-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full mt-4">
+                Sign in
+              </Button>
+            </form>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <Button variant="outline" className="w-full">
+              <Chrome className="mr-2 h-4 w-4" />
+              Google
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
